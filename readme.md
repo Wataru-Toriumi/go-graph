3b152f9bb82d622687be4e6a7ef82bc157fe3575
Wataru-Toriumi <wataru.19940213@gmail.com>
2021-05-09 23:00:27 +0900

    feat: add weight property to node and edge

--- readme.md
+++ readme.md
@@ -25,7 +25,6 @@ func main(){
 }
 ```
 
-
 ## Test
 
 ```bash
--- src/graph/graph.go
+++ src/graph/graph.go
@@ -4,32 +4,43 @@ import (
 	"fmt"
 )
 
+type Node struct {
+	Id string
+	Weight map[string] int
+}
+
+type Edge struct {
+	From string
+	To string
+	Weight map[string] int
+}
+
 type Graph struct {
-	nodes []string
-	edges [][]string
+	nodes []Node
+	edges []Edge
 }
 
 type GraphInterface interface {
-	get_nodes() []string
-	get_edges() [][]string
-	add_nodes([]string)
-	add_edges([][]string)
+	get_nodes() []Node
+	get_edges() []Edge
+	add_nodes([]Node)
+	add_edges([]Edge)
 	show()
 }
 
-func (G Graph) get_nodes() []string {
+func (G Graph) get_nodes() []Node {
 	return G.nodes
 }
 
-func (G Graph) get_edges() [][]string {
+func (G Graph) get_edges() []Edge {
 	return G.edges
 }
 
-func (G *Graph) add_nodes(nodes []string) {
+func (G *Graph) add_nodes(nodes []Node) {
 	G.nodes = append(G.nodes, nodes...)
 }
 
-func (G *Graph) add_edges(edges [][]string) {
+func (G *Graph) add_edges(edges []Edge) {
 	G.edges = append(G.edges, edges...)
 }
 
@@ -38,24 +49,24 @@ func (G Graph) show() {
 	fmt.Printf("edges: %v\n", G.edges)
 }
 
-func New (nodes []string, edges [][]string) *Graph {
+func New (nodes []Node, edges []Edge) *Graph {
 	return &Graph{nodes, edges}
 }
 
 
-func Get_nodes(g GraphInterface) []string {
+func Get_nodes(g GraphInterface) []Node {
 	return g.get_nodes()
 }
 
-func Get_edges(g GraphInterface) [][]string {
+func Get_edges(g GraphInterface) []Edge {
 	return g.get_edges()
 }
 
-func Add_nodes(g GraphInterface, nodes []string) {
+func Add_nodes(g GraphInterface, nodes []Node) {
 	g.add_nodes(nodes)
 }
 
-func Add_edges(g GraphInterface, edges [][]string) {
+func Add_edges(g GraphInterface, edges []Edge) {
 	g.add_edges(edges)
 }
 
